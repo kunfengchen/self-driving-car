21c44ce9935a4c7fb8799beac3a32daa9f6b6de0
kunfengchen <kunfengchen@live.com>
2016-10-14 23:11:34 -0700

    Create docker-run.sh

---
+++ docker/docker-run.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+xhost +
+
+docker run -it --rm \
+   --net host \
+   --env="DISPLAY" \
+   --env="QT_X11_NO_MITSHM=1"
+   --volume "$HOME/.Xauthority:/root/.Xauthority:rw" \
+   --volume "$HOME/sharefolder:/sharefolder:shared" \
+   --volume /dev/video0:/dev/video0 \
+   kfc:u14-ros-indigo-autoware /bin/bash
