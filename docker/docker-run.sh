4e94be15fba08666dfe5dd334348ca36e32fc112
kunfengchen <kunfengchen@live.com>
2016-10-15 00:02:25 -0700

    Update docker-run.sh

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
 
-xhost +
-
 docker run -it --rm \
    --net host \
    --env="DISPLAY" \
-   --env="QT_X11_NO_MITSHM=1" \
    --volume "$HOME/.Xauthority:/root/.Xauthority:rw" \
    --volume "$HOME/sharefolder:/sharefolder" \
-   --volume /dev/video0:/dev/video0 \
    kunfengchen:u14-ros-indigo-autoware /bin/bash
